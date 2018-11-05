5cc6d9a03f5905e9780d32f21a50884325ba86a2
Daniel Wulff <danielwulff.ee@gmail.com>
2018-11-04 20:51:12 -0800

    add interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 // You can use a relative import, i.e. `import 'category_route.dart;'` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'package:hello_rectangle/category_screen.dart';
+import 'category_screen.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -25,7 +25,15 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      // TODO: Fill out the theme parameter
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
       home: CategoryScreen(),
     );
   }
